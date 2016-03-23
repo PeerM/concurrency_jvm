db60ee471f32bf46237a042c03588e9594c26d56
Ferdinand Peer Mathia <ferdinandpeer.mathia@hs-augsburg.de>
2016-03-23 14:56:54 +0100

    implemented 1.1 but stop removes all balls

--- src/main/java/de/hs_augsburg/nlp/one/BounceBallAdjustable.java
+++ src/main/java/de/hs_augsburg/nlp/one/BounceBallAdjustable.java
@@ -1,15 +1,19 @@
 package de.hs_augsburg.nlp.one;
 
 import javax.swing.*;
+import javax.vecmath.Vector2d;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
+import static java.lang.Math.round;
+
 public class BounceBallAdjustable {
     // Atomic reference of a Immutable Persistent HashMap of Balls
-    private final ConcurrentHashMap<Integer,Ball> allBalls = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Integer, Ball> allBalls = new ConcurrentHashMap<>();
+    private java.util.List<BallMover> movers = new LinkedList<>();
     private final Box box;
     private int currentBallIndex = 1;
 
@@ -21,6 +25,12 @@ public class BounceBallAdjustable {
         JButton b;
         p.add(b = new JButton("Start"));
         b.addActionListener(new CommandListener(CommandListener.NEWBALL));
+        p.add(b = new JButton("Faster"));
+        b.addActionListener(new CommandListener(CommandListener.FASTER));
+        p.add(b = new JButton("Slower"));
+        b.addActionListener(new CommandListener(CommandListener.SLOWER));
+        p.add(b = new JButton("Stop"));
+        b.addActionListener(new CommandListener(CommandListener.STOP));
         p.add(b = new JButton("Close"));
         b.addActionListener(new CommandListener(CommandListener.EXIT));
         frame.add("South", p);
@@ -35,10 +45,15 @@ public class BounceBallAdjustable {
 
     protected void makeAndStartBall() {
         Ball b = new Ball(Color.black);
-        allBalls.put(currentBallIndex,b);
+        allBalls.put(currentBallIndex, b);
         BallMover mover = new BallMover(currentBallIndex, allBalls);
+        movers.add(mover);
         new Thread(mover).start();
         currentBallIndex += 1;
+    }
+
+    void stopAllMovementAndProcessing(){
+
     }
 
     class Box extends JPanel {
@@ -58,6 +73,9 @@ public class BounceBallAdjustable {
     class CommandListener implements ActionListener {
         static final int EXIT = 0;
         static final int NEWBALL = 1;
+        static final int FASTER = 2;
+        static final int SLOWER = 3;
+        static final int STOP = 4;
         private int command; // der Listener merkt sich seine Aufgabe
 
         public CommandListener(int command) {
@@ -68,6 +86,17 @@ public class BounceBallAdjustable {
             switch (command) {
                 case EXIT:
                     System.exit(0);
+                    break;
+                case FASTER:
+                    allBalls.replaceAll((k, v) -> v.adjustSpeed(2));
+                    break;
+                case SLOWER:
+                    allBalls.replaceAll((k, v) -> v.adjustSpeed(-1));
+                    break;
+                case STOP:
+                    allBalls.clear();
+                    box.repaint();
+                    break;
                 case NEWBALL:
                     makeAndStartBall();
                     break;
@@ -89,7 +118,10 @@ public class BounceBallAdjustable {
         public void run() {
             try {
                 for (int i = 1; i <= 1000; i++) {
-                    work();
+                    Ball r = ballAtom.computeIfPresent(ballIdentity, (k, v) -> v.move());
+                    if (r == null){
+                        return;
+                    }
                     Thread.sleep(5); // cpu-schonendes Warten
                     box.repaint();
                 }
@@ -98,10 +130,6 @@ public class BounceBallAdjustable {
                 e.printStackTrace();
             }
             // allBalls.remove(this); // ball leaves animation quietly
-        }
-
-        private void work(){
-            ballAtom.compute(ballIdentity,(k,v)-> v.move());
         }
     }
 
@@ -154,7 +182,7 @@ public class BounceBallAdjustable {
                 newY = 0;
                 newDy = -newDy;
             }
-            if (y + YSIZE >= d.height) {
+            if (newY + YSIZE >= d.height) {
                 newY = d.height - YSIZE;
                 newDy = -newDy;
             }
@@ -162,9 +190,12 @@ public class BounceBallAdjustable {
         }
 
         public Ball adjustSpeed(int deltaSpeed) {
-            int newDx = dx + deltaSpeed;
-            int newDy = dy + deltaSpeed;
-            return new Ball(this.color, this.x, this.y, newDx, newDy);
+            Vector2d oldSpeed = new Vector2d(new double[]{dx, dy});
+            Vector2d newSpeed = new Vector2d(new double[]{dx, dy});
+            newSpeed.normalize();
+            newSpeed.scaleAdd(deltaSpeed, oldSpeed);
+
+            return new Ball(this.color, this.x, this.y, (int) round(newSpeed.x), (int) round(newSpeed.y));
         }
 
     }
