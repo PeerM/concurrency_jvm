f03c530fe78a1d33e980b6818b720c4dba69d7b9
Ferdinand Peer Mathia <ferdinandpeer.mathia@hs-augsburg.de>
2016-03-24 20:13:07 +0100

    added nice comment

--- src/main/java/de/hs_augsburg/nlp/one/BounceBallAdjustable.java
+++ src/main/java/de/hs_augsburg/nlp/one/BounceBallAdjustable.java
@@ -130,6 +130,7 @@ public class BounceBallAdjustable {
                     if (Thread.currentThread().isInterrupted()){
                         return;
                     }
+                    // potential for a race condition if the thread is interrupted here
                     Ball r = ballAtom.compute(ballIdentity, (k, v) -> v.move());
                     Thread.sleep(5); // cpu-schonendes Warten
                     box.repaint();
