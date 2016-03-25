c648b04159537429d3eff02a4d46c467bb322877
Ferdinand Peer Mathia <ferdinandpeer.mathia@hs-augsburg.de>
2016-03-25 17:30:15 +0100

    nicer

--- nlpclojure/src/nlpclojure/one/parallelPrime.clj
+++ nlpclojure/src/nlpclojure/one/parallelPrime.clj
@@ -7,7 +7,7 @@
                          (< number 2) false
                          (= number 2) true
                          (= (mod number 2) 0) false
-                         #(true)
+                         true
                          (let [checkRange (range 3 (/ (+ (Math/sqrt number) 1) 2) 2)]
                            (not
                              (some
