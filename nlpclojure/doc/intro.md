ede21469fb4e4c9ffc3a456d0ed807667344f8eb
Ferdinand Peer Mathia <ferdinandpeer.mathia@hs-augsburg.de>
2016-03-25 14:42:30 +0100

    added clojure project

---
+++ nlpclojure/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to nlpclojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ nlpclojure/test/nlpclojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns nlpclojure.core-test
+  (:require [clojure.test :refer :all]
+            [nlpclojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
