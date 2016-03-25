c23b2f7e4c10e4b1405bce3cb7f7de437620e45a
Ferdinand Peer Mathia <ferdinandpeer.mathia@hs-augsburg.de>
2016-03-25 18:50:22 +0100

    now with parallel tasks

--- nlpclojure/src/nlpclojure/one/parallelPrime.clj
+++ nlpclojure/src/nlpclojure/one/parallelPrime.clj
@@ -1,18 +1,25 @@
 (ns nlpclojure.one.parallelPrime)
 ;;(:require [clojure.math.numeric-tower :as math]))
 
-
+(def interval (Math/pow 10 8))
 
 (defn isPrime [number] (cond
                          (< number 2) false
                          (= number 2) true
                          (= (mod number 2) 0) false
                          true
-                         (let [checkRange (range 3 (/ (+ (Math/sqrt number) 1) 2) 2)]
-                           (not
-                             (->>
-                               checkRange
-                               (map
-                                 (fn [divider] (= 0 (mod number divider))))
-                               (some
-                                 true?))))))
+                         (not
+                           (->>
+                             (iterate
+                               (fn [[_ end]] [end (+ end interval)])
+                               [3 (+ 3 interval)])
+                             (take-while (fn [[start end]] (<= (* start start) number)))
+                             (pmap
+                               (fn [[start end]] (->>
+                                                   (range start end 2)
+                                                   (take-while
+                                                     (fn [i] (<= (* i i) number)))
+                                                   (some
+                                                     (fn [divider] (= 0 (mod number divider)))))))
+                             (some
+                               true?)))))
