73a89c3feda9b681e0e60e23d6dd3d3301d6ca8b
Ferdinand Peer Mathia <ferdinandpeer.mathia@hs-augsburg.de>
2016-03-26 23:53:45 +0100

    Clojure! we'r fast now! WOOH Partey

--- nlpclojure/src/clojure/de/hs_augsburg/nlp/one/parallelPrime.clj
+++ nlpclojure/src/clojure/de/hs_augsburg/nlp/one/parallelPrime.clj
@@ -1,10 +1,20 @@
 (ns de.hs_augsburg.nlp.one.parallelPrime
   (:import (de.hs_augsburg.nlp.one PrimeChecker)))
 
-(def interval (Math/pow 10 8))
+;;(def interval (Math/pow 10 8))
+;; long q = (Math.round(Math.sqrt(number) + 1) / 1000000L);
+;; return Math.max(q, 500);
+(defn longMax [^long a ^long b] (Math/max a b))
+(defn calcInterval [^long number]
+  (longMax
+    10
+    (/
+      (+ 1
+        (Math/round
+          (Math/sqrt number)))
+      1000)))
 
-
-(defn nextSeqment [number, inter, [prevStart prevEnd]]
+(defn nextSeqment [number, inter, [_ prevEnd]]
   (let [nextEnd (+ prevEnd inter)]
     (if
       (> (* nextEnd nextEnd) number)
@@ -16,13 +26,14 @@
                          (= number 2) true
                          (= (mod number 2) 0) false
                          true
-                         (not
-                           (->>
-                             (iterate
-                               (fn [task] (nextSeqment number interval task))
-                               (nextSeqment number interval [0 3]))
-                             (take-while (fn [[start _]] (<= (* start start) number)))
-                             (pmap
-                               (fn [[start end]] (PrimeChecker/doesRangeContainDivider start end number)))
-                             (some
-                               true?)))))
+                         (let [interval (calcInterval number)]
+                           (not
+                             (->>
+                               (iterate
+                                 (fn [task] (nextSeqment number interval task))
+                                 (nextSeqment number interval [0 3]))
+                               (take-while (fn [[start _]] (<= (* start start) number)))
+                               (pmap
+                                 (fn [[start end]] (PrimeChecker/doesRangeContainDivider start end number)))
+                               (some
+                                 true?))))))
--- nlpclojure/src/clojure/de/hs_augsburg/nlp/one/primePlayground.clj
+++ nlpclojure/src/clojure/de/hs_augsburg/nlp/one/primePlayground.clj
@@ -3,4 +3,6 @@
             [de.hs_augsburg.nlp.one.parallelPrime :refer :all]))
 
 ;;(time (isPrime 1000000000000000003))
-(print (str "1117 should be a prime and it is :" (isPrime 1117)))
+;;(print (str "1117 should be a prime and it is :" (isPrime 1117)))
+
+(time (doall(map isPrime (range 1000000000000000000 1000000000000000005))))
