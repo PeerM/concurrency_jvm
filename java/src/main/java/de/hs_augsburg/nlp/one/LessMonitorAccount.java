b86e9ff531ad45c338aa5824d5b3009226032a0b
Stephan Reichinger <stephan.reichinger@hs-augsburg.de>
2016-04-03 18:16:20 +0200

    added AccountOserver and LessMonitorAccoutn classes

---
+++ java/src/main/java/de/hs_augsburg/nlp/one/LessMonitorAccount.java
@@ -0,0 +1,29 @@
+package de.hs_augsburg.nlp.one;
+
+import banking.accounts.Account;
+
+public class LessMonitorAccount implements Account {
+
+    private int balance;
+
+    public synchronized void  deposit(int i) {
+        balance += i;
+    }
+
+    public synchronized boolean withdraw(int i) {
+        if (balance >= i) { // don't allow overdrawing
+            balance -= i;
+            return true;
+        }
+        return false;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+    
+    public synchronized String toString() {
+        return "Konto mit Stand: " + balance;
+    }
+}
+
