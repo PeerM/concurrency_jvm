88b510bd9e6bf77bc9a1c76b7d20241ec603c7ce
Ferdinand Peer Mathia <ferdinandpeer.mathia@hs-augsburg.de>
2016-04-09 21:57:10 +0200

    added faulty account with long adder

---
+++ java/src/main/java/de/hs_augsburg/nlp/one/account/AdderAccount.java
@@ -0,0 +1,41 @@
+package de.hs_augsburg.nlp.one.account;
+
+import de.hs_augsburg.meixner.account.Account;
+
+import java.util.concurrent.atomic.LongAdder;
+
+public class AdderAccount implements Account {
+
+    private volatile LongAdder balance;
+
+    public AdderAccount() {
+        balance = new LongAdder();
+    }
+
+    public synchronized void deposit(int i) {
+        balance.add(i);
+    }
+
+    public synchronized boolean withdraw(int i) {
+
+        if (balance.sum() >= i) {
+            // Warning: this smells like check then act
+            // This will not leave the total balance inconsistent
+            // but it might allow overdrawing if the one adds negative, while an other ones sums up for the check
+            // this could be considered a semantic change from the.
+            // LongAdder Only gives us quiescent consistency
+            balance.add(i * -1);
+            return true;
+        }
+        return false;
+    }
+
+    public int getBalance() {
+        return balance.intValue();
+    }
+
+    public synchronized String toString() {
+        return "Konto mit Stand: " + balance.sum();
+    }
+}
+
