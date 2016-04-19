c519be21048e3788c23c99d01cb988ce6a34ab67
Ferdinand Mathia <FerdinandPeer.Mathia@HS-Augsburg.DE>
2016-04-19 19:24:05 +0200

    better running

--- java/src/test/java/de/hs_augsburg/nlp/one/BenchUnified.java
+++ java/src/test/java/de/hs_augsburg/nlp/one/BenchUnified.java
@@ -4,15 +4,15 @@ import de.hs_augsburg.meixner.account.Account;
 import de.hs_augsburg.meixner.account.MonitorAccount;
 import de.hs_augsburg.nlp.one.account.AtomicAdderAccount;
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 public class BenchUnified {
 
 
     @Benchmark
-    @Fork(1)
-    @BenchmarkMode(Mode.Throughput)
-    @Warmup(iterations = 4)
-    @Measurement(iterations = 8)
     @Threads(4)
     public void deposit(BenchmarkState state) {
         Account acc = state.account;
@@ -20,16 +20,19 @@ public class BenchUnified {
     }
 
     @Benchmark
-    @Fork(1)
-    @BenchmarkMode(Mode.Throughput)
-    @Warmup(iterations = 4)
-    @Measurement(iterations = 8)
-    @Threads(4)
     @OperationsPerInvocation(2)
     public void depositAndCheck(BenchmarkState state) {
         Account acc = state.account;
         acc.deposit(1);
         acc.getBalance();
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(2)
+    public void depositAndWithdraw(BenchmarkState state) {
+        Account acc = state.account;
+        acc.deposit(1);
+        acc.withdraw(1);
     }
 
     @State(Scope.Benchmark)
@@ -51,5 +54,23 @@ public class BenchUnified {
                     throw new IllegalArgumentException("impl '" + impl + "' not supported");
             }
         }
+    }
+
+
+    // to play with JMH without plugins or custom runners
+    public static void main(String[] args) throws RunnerException {
+        // this is the config, you can play around with this
+        Options opt = new OptionsBuilder()
+                .include(BenchUnified.class.getSimpleName()+".depositAndWithdraw")
+                .param("impl","Monitor","AtomicAdder")
+                .forks(1)
+                .warmupIterations(4)
+                .measurementIterations(8)
+                .mode(Mode.Throughput)
+                .threads(4)
+                .build();
+
+        new Runner(opt).run();
+
     }
 }
