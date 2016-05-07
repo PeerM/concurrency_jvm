e942ebc54f96ac42cb46305daefa146759936ad9
Ferdinand Peer Mathia <ferdinandpeer.mathia@hs-augsburg.de>
2016-05-07 19:04:30 +0200

    continued switch to binary and

--- java/src/main/java/de/hs_augsburg/nlp/three/radix/PHistRadixSort.java
+++ java/src/main/java/de/hs_augsburg/nlp/three/radix/PHistRadixSort.java
@@ -3,21 +3,24 @@ package de.hs_augsburg.nlp.three.radix;
 
 import java.util.Random;
 
+
+@SuppressWarnings("Duplicates")
 public class PHistRadixSort implements ISort {
     @Override
     public int[] sort(int[] a) {
         int RADIX = 8;
         int RADICES = 1 << RADIX;
+        int mask = (1 << RADIX) - 1;
         for (int bits = 0; bits < 32; bits += RADIX) {
             int[] sortedData = new int[a.length];
             // 1st step: Calculate histogram with RADICES entries (RADICES = 1<<RADIX)
-            int[] histogram = histogram(a, RADICES, bits);
+            int[] histogram = histogram(a, RADICES, bits, mask);
 
             // 2nd step: Prescan the histogram bucket */
             stepTwo(RADICES, histogram);
 
             // 3rd step: Rearrange the elements based on prescaned histogram */
-            stepThree(a, RADICES, bits, sortedData, histogram);
+            stepThree(a, bits, sortedData, histogram, mask);
             a = sortedData;
         }
         return a;
@@ -32,16 +35,16 @@ public class PHistRadixSort implements ISort {
         }
     }
 
-    private void stepThree(int[] a, int RADICES, int bits, int[] sortedData, int[] histogram) {
+    private void stepThree(int[] a, int bits, int[] sortedData, int[] histogram, int mask) {
         for (int i = 0; i < a.length; ++i) {
-            sortedData[histogram[(a[i] >> bits) % RADICES]++] = a[i];
+            sortedData[histogram[(a[i] >> bits) & mask]++] = a[i];
         }
     }
 
-    public static int[] histogram(int[] a, int RADICES, int bit) {
+    public static int[] histogram(int[] a, int RADICES, int bit, int mask) {
         int[] result = new int[RADICES];
         for (int i = 0; i < a.length; i++) {
-            result[(a[i] >> bit) % RADICES]++;
+            result[(a[i] >> bit) & mask]++;
         }
         return result;
     }
@@ -55,11 +58,11 @@ public class PHistRadixSort implements ISort {
         for (int i = 0; i < 100000; i++) {
             sum += random.nextInt();
         }
-        PHistRadixSort radixSort = new PHistRadixSort();
+        SequentialRadixSort radixSort = new SequentialRadixSort();
         doWork(random, radixSort);
     }
 
-    private static void doWork(Random random, PHistRadixSort radixSort) {
+    private static void doWork(Random random, SequentialRadixSort radixSort) {
         System.out.println("starting");
         for (int i = 0; i < 50; i++) {
             int[] ints = random.ints(10000000, 0, Integer.MAX_VALUE).toArray();
