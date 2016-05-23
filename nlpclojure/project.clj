cb05a039f1c1ecb471780fb4865b2c01d4343539
Ferdinand Peer Mathia <ferdinandpeer.mathia@hs-augsburg.de>
2016-05-23 15:26:49 +0200

    deleted old module

--- nlpclojure/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject nlpclojure "1.2.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :source-paths ["src/clojure"]
-  :java-source-paths ["src/java"]
-  :dependencies [[org.clojure/clojure "1.8.0"]])
