6bd8e5f8a92a69fbad0c7f54f29200eb5eee97be
Ferdinand Peer Mathia <ferdinandpeer.mathia@hs-augsburg.de>
2016-05-23 15:25:33 +0200

    moved old clojure stuff into proper project

--- java/src/main/clojure/de/hs_augsburg/nlp/one/parallelPrime.clj
+++ java/src/main/clojure/de/hs_augsburg/nlp/one/parallelPrime.clj
@@ -1,5 +1,5 @@
 (ns de.hs_augsburg.nlp.one.parallelPrime
-  (:import (de.hs_augsburg.nlp.one PrimeChecker)))
+  (:import (de.hs_augsburg.nlp.one.prime PrimeCheckerCj)))
 
 (defn longMax [^long a ^long b] (Math/max a b))
 (defn calcInterval [^long number]
@@ -28,6 +28,6 @@
                                      (nextSeqment number interval [0 3]))
                                    (take-while (fn [[start _]] (<= (* start start) number)))
                                    (pmap
-                                     (fn [[start end]] (PrimeChecker/doesRangeContainDivider start end number)))
+                                     (fn [[start end]] (PrimeCheckerCj/doesRangeContainDivider start end number)))
                                    (some
                                      true?))))))
--- java/src/main/java/de/hs_augsburg/nlp/one/prime/PrimeCheckerCj.java
+++ java/src/main/java/de/hs_augsburg/nlp/one/prime/PrimeCheckerCj.java
@@ -1,6 +1,6 @@
-package de.hs_augsburg.nlp.one;
+package de.hs_augsburg.nlp.one.prime;
 
-public class PrimeChecker {
+public class PrimeCheckerCj {
     public static boolean doesRangeContainDivider(long start, long end, long number) {
         for (long i = start; i < end; i += 2) {
             if (number % i == 0)
