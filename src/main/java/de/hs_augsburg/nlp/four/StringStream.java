1132aa8ef666604bb52f003afae4cd8a072dfc9d
Stephan Reichinger <stephan.reichinger@hs-augsburg.de>
2016-05-24 15:27:57 +0200

    Sequential version of StringStream implemented

---
+++ src/main/java/de/hs_augsburg/nlp/four/StringStream.java
@@ -0,0 +1,46 @@
+package de.hs_augsburg.nlp.four;
+
+import de.hs_augsburg.nlp.three.histogram.ClojureHelpers;
+import org.apache.commons.io.IOUtils;
+
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.stream.Stream;
+
+
+
+public class StringStream {
+    public Collection<String> strings;
+    public Stream stream;
+
+    public static void main(String[] args) {
+        StringStream stringStream = new StringStream();
+        Stream<String> words = stringStream.getWords("pride.txt");
+    }
+
+    public Stream<String> getWords(String filePath)
+    {
+        Stream<String> res;
+        try {
+            res = IOUtils.readLines(getClass().getClassLoader().getResourceAsStream(filePath), StandardCharsets.UTF_8).stream();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+        return res;
+    }
+
+    public int[] makeHistogram(Stream<String> words){
+        int[] hist = words
+                .flatMap(word -> word.chars().boxed()).parallel()
+                .collect(
+                        () -> new int[256],
+                        (int[] ints, Integer integer) -> {
+                            ints[integer]++;
+                        },
+                        ClojureHelpers::arrayElementBasedAdd);
+//        System.out.println(hist);
+        return hist;
+    }
+}
