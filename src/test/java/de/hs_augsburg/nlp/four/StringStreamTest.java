cee4448ee34bdfa2a32c695355cb505ed255b953
Stephan Reichinger <stephan.reichinger@hs-augsburg.de>
2016-05-25 15:15:21 +0200

    added test

--- src/test/java/de/hs_augsburg/nlp/four/StringStreamTest.java
+++ src/test/java/de/hs_augsburg/nlp/four/StringStreamTest.java
@@ -2,19 +2,25 @@ package de.hs_augsburg.nlp.four;
 
 
 import de.hs_augsburg.nlp.four.streams.StringStream;
+import de.hs_augsburg.nlp.four.streams.StringStreamParallel;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Collection;
 import java.util.LinkedList;
 
+import static org.junit.Assert.*;
+
 public class StringStreamTest {
     public StringStream stringStream;
+    public StringStreamParallel stringStreamParallel;
     public Collection<String> words;
 
     @Before
     public void setUp() {
         this.stringStream = new StringStream();
+        this.stringStreamParallel = new StringStreamParallel();
         this.words = new LinkedList<>();
         words.add("aaaa");
         words.add("aaaa");
@@ -27,6 +33,14 @@ public class StringStreamTest {
 
     @Test
     public void stringStreamCorrectnessTest() {
-        stringStream.makeHistogram(words.stream());
+        int[] res = stringStream.makeHistogram(words.stream());
+        assertEquals(res[97], 36);
     }
+
+    @Test
+    public void stringStreamParallelTest() {
+        int[] res = stringStream.makeHistogram(words.stream());
+        assertEquals(res[97], 36);
+    }
+
 }
