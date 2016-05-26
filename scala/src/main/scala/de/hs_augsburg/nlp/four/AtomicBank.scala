d806f88d0bf8bc5b6208a7217d31568f372eb10a
Ferdinand Mathia <FerdinandPeer.Mathia@HS-Augsburg.DE>
2016-05-26 22:25:30 +0200

    prettied up the scala code

--- scala/src/main/scala/de/hs_augsburg/nlp/four/AtomicBank.scala
+++ scala/src/main/scala/de/hs_augsburg/nlp/four/AtomicBank.scala
@@ -2,43 +2,42 @@ package de.hs_augsburg.nlp.four
 
 import java.lang.Long
 import java.util
-import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.atomic.{AtomicLong, AtomicReference}
 
+//implicit class for conversion of scala function to java UnaryOperator
+import de.hs_augsburg.nlp.four.UnaryOpScala.UnaryOpScala
 import de.hs_augsburg.nlp.two.BasicMonitor.Entry
-import de.hs_augsburg.nlp.two.Functional.{AtomicNumberGenerator, ImmutableAccount}
 
 import scala.collection.JavaConverters._
 
 class AtomicBank {
-  val state = new AtomicReference(Map[Long, ImmutableAccount]())
-  val accnoGen = new AtomicNumberGenerator()
+  val state = new AtomicReference(Map[Long, ScalaAccount]())
+  val accNoGen = new AtomicLong()
 
   def deposit(accNo: Long, amount: Int): Unit =
-    state.updateAndGet(new UnaryOpScala[Map[Long, ImmutableAccount]](
-      (last) => last + (accNo -> last.get(accNo).get.deposit(amount, "withdraw"))))
+    state.updateAndGet((last: Map[Long, ScalaAccount]) => last + (accNo -> last.get(accNo).get.deposit(amount, "withdraw")))
 
-  def getBalance(accNo: Long): Int = state.get().get(accNo).get.currentBalance()
+  def getBalance(accNo: Long): Int = state.get().get(accNo).get.balance
 
   def createAccount(): Long = {
-    val accno = accnoGen.getNext
-    state.updateAndGet(new UnaryOpScala[Map[Long, ImmutableAccount]]((last) => last updated(accno, new ImmutableAccount())))
-    accno
+    val accNo = accNoGen.getAndIncrement()
+    state.updateAndGet(new UnaryOpScala[Map[Long, ScalaAccount]]((last) => last updated(accNo, new ScalaAccount())))
+    accNo
   }
 
   def transfer(from: Long, to: Long, amount: Int): Unit =
-    state.updateAndGet(new UnaryOpScala[Map[Long, ImmutableAccount]](
-      (last) => last +(from -> last.get(from).get.withdraw(amount, "transfer-> withdraw"),
-        to -> last.get(to).get.deposit(amount, "transfer-> deposit"))))
+    state.updateAndGet((last: Map[Long, ScalaAccount]) => last +
+      (from -> last.get(from).get.withdraw(amount, "transfer-> withdraw"),
+        to -> last.get(to).get.deposit(amount, "transfer-> deposit")))
 
 
   def withdraw(accNo: Long, amount: Int): Unit =
-    state.updateAndGet(new UnaryOpScala[Map[Long, ImmutableAccount]](
-      (last) => last + (accNo -> last.get(accNo).get.withdraw(amount, "withdraw"))))
+    state.updateAndGet((last: Map[Long, ScalaAccount]) => last + (accNo -> last.get(accNo).get.withdraw(amount, "withdraw")))
 
-  def getAccountEntries(accNo: Long): util.List[Entry] = state.get().get(accNo).get.getEntries
+  def getAccountEntries(accNo: Long): util.List[Entry] = state.get().get(accNo).get.entries.asJava
 
   def getAccountEntries(accNos: util.List[Long]): util.List[Entry] = {
     val localState = state.get()
-    accNos.asScala.flatMap(e => localState.get(e).get.getEntries.asScala).asJava
+    accNos.asScala.flatMap(e => localState.get(e).get.entries).asJava
   }
 }
--- scala/src/main/scala/de/hs_augsburg/nlp/four/UnaryOpScala.scala
+++ scala/src/main/scala/de/hs_augsburg/nlp/four/UnaryOpScala.scala
@@ -3,6 +3,14 @@ package de.hs_augsburg.nlp.four
 import java.util.function.UnaryOperator
 
 
-class UnaryOpScala[T](val function: (T) => T) extends UnaryOperator[T] {
-  override def apply(t: T): T = function(t)
+object UnaryOpScala {
+
+  /***
+    * implicit class for conversion of scala function to java UnaryOperator
+    * @param function the function to map to a UnaryOperator
+    * @tparam T type of the operator
+    */
+  implicit class UnaryOpScala[T](val function: (T) => T) extends UnaryOperator[T] {
+    override def apply(t: T): T = function(t)
+  }
 }
