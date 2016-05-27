4604dc4cc228bc306c96eef866636069a31d9156
Ferdinand Mathia <FerdinandPeer.Mathia@HS-Augsburg.DE>
2016-05-27 22:02:55 +0200

    compacted code

--- src/main/java/de/hs_augsburg/nlp/four/mnemonic/JavaCoder.java
+++ src/main/java/de/hs_augsburg/nlp/four/mnemonic/JavaCoder.java
@@ -1,31 +1,34 @@
 package de.hs_augsburg.nlp.four.mnemonic;
 
 import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableMap;
 import one.util.streamex.IntStreamEx;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.pcollections.ConsPStack;
 import org.pcollections.HashTreePSet;
-import org.pcollections.PSet;
 import org.pcollections.PStack;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+@SuppressWarnings("WeakerAccess")
 public class JavaCoder {
     private final Map<String, String> charCode;
     private final Map<String, List<String>> wordsForNum;
-    private Map<String, String> mnemonics;
 
     public JavaCoder(List<String> dictionary) {
-        mnemonics = new HashMap<>(8);
-        mnemonics.put("2", "ABC");
-        mnemonics.put("3", "DEF");
-        mnemonics.put("4", "GHI");
-        mnemonics.put("5", "JKL");
-        mnemonics.put("6", "MNO");
-        mnemonics.put("7", "PQRS");
-        mnemonics.put("8", "TUV");
-        mnemonics.put("9", "WXYZ");
+        Map<String, String> mnemonics = ImmutableMap.<String, String>builder()
+                .put("2", "ABC")
+                .put("3", "DEF")
+                .put("4", "GHI")
+                .put("5", "JKL")
+                .put("6", "MNO")
+                .put("7", "PQRS")
+                .put("8", "TUV")
+                .put("9", "WXYZ")
+                .build();
         charCode = mnemonics.entrySet().stream()
                 .flatMap(
                         entry -> entry.getValue()
@@ -51,28 +54,11 @@ public class JavaCoder {
         }
         return IntStreamEx
                 .range(1, number.length() + 1)
-                .flatMapToObj(i -> {
-                    String first = number.substring(0, i);
-                    String second = number.substring(i, number.length());
-                    List<String> possible_words = wordsForNum.getOrDefault(first, ConsPStack.empty());
-                    return possible_words
-                            .stream()
-                            .flatMap(word -> encode(second)
-                                    .stream()
-                                    .map((PStack<String> rest) -> rest.plus(word)));
-                }).collect(Collectors.toSet());
-//        PSet<PStack<String>> accumulator = HashTreePSet.empty();
-//        for (int i = 1; i < number.length() + 1; i++) {
-//            String first = number.substring(0, i);
-//            String second = number.substring(i, number.length());
-//            List<String> possible_words = wordsForNum.getOrDefault(first, ConsPStack.empty());
-//            for (String word : possible_words) {
-//                for (PStack<String> rest : encode(second)) {
-//                    accumulator = accumulator.plus(rest.plus(word));
-//                }
-//            }
-//        }
-//        return accumulator;
+                .flatMapToObj(i -> wordsForNum.getOrDefault(number.substring(0, i), ConsPStack.empty())
+                        .stream()
+                        .flatMap(word -> encode(number.substring(i, number.length()))
+                                .stream()
+                                .map((PStack<String> rest) -> rest.plus(word)))).collect(Collectors.toSet());
     }
 
 }
